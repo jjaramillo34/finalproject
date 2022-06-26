3214ca2551f9c13819d79f8552096a48e2eb51be
Doneuno <javiermanzur89@gmail.com>
2022-06-26 19:33:31 -0300

    contact changes

--- contact/forms.py
+++ contact/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm
 from .models import Contact
 
@@ -5,3 +6,8 @@ class ContactForm(ModelForm):
     class Meta:
         model = Contact
         fields = '__all__'
+        widget = {
+            'email': forms.TextInput(attrs={'class' : 'form-group'}),
+            'subject': forms.TextInput(attrs={'class' : 'form-group'}),
+            'message': forms.Textarea(attrs={'class' : 'form-group'}),
+        }
