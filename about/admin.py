50562937af68272eae5088fd4abe7bf849c6d1e8
Javier Jaramillo <jjaramillo34@gmail.com>
2022-06-26 15:20:25 -0400

    Update requirements.txt

--- about/admin.py
+++ about/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from about.models import About
+from about.models import About, Members
 
 # Register your models here.
 admin.site.register(About)
+admin.site.register(Members)
 
