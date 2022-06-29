528ec9bf41715ece41676545185275f90a2d761e
Javier Jaramillo <jjaramillo34@gmail.com>
2022-06-29 10:15:01 -0400

    Update contact form generic formview

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views import generic
 from contact.forms import ContactForm
+from django.contrib import messages
+
+class ContactFormView(generic.FormView):
+    form_class = ContactForm
+    template_name = 'contact/contact.html'
+    
+    def get_success_url(self) -> str:
+        return self.request.path
+    
+    def form_valid(self, form):
+        form.save()
+        messages.add_message(self.request, messages.INFO, 'You Message Has Been Send Successfully')
+        return super().form_valid()
+    
+    def form_invalid(self, form):
+        form.add_error(None, 'Oops... something went wrong with the form')
+        return super().form_invalid(form)
     
 def contact_view(request):
     header = "Contact Us"
