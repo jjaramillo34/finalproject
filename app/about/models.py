8a527fab5e92c7198f95d191f96b54e7c9b09eed
Javier Jaramillo <jjaramillo34@gmail.com>
2022-07-13 17:32:41 -0400

    Fixed docked and added internationalization

--- app/about/models.py
+++ app/about/models.py
@@ -1,12 +1,22 @@
 from django.db import models
+from parler.models import TranslatableModel, TranslatedFields
 
-class About(models.Model):
-    team1 = models.TextField()
-    team2 = models.TextField()
-
-class Members(models.Model):
-    name = models.CharField(max_length=50, default='/')
-    github = models.CharField(max_length=250, default='www.github.com')
-    profession = models.CharField(max_length=250, default='/')
-    img = models.ImageField(upload_to='teams')
+class About(TranslatableModel):
+    translations = TranslatedFields(
+        team1 = models.TextField(),
+        team2 = models.TextField(),
+    )
     
+    def __str__(self) -> str:
+        return self.team1
+
+class Members(TranslatableModel):
+    translations = TranslatedFields(
+        name = models.CharField(max_length=50, default='/'),
+        github = models.CharField(max_length=250, default='www.github.com'),
+        profession = models.CharField(max_length=250, default='/'),
+        img = models.ImageField(upload_to='teams'),
+    )
+    
+    def __str__(self):
+        return self.name
